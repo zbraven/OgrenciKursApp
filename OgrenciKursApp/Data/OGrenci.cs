f2acaded9739f3c03649ef42c617d01555d8e8c5
Zahid Buğra Sarıcan <zbsarican@gmail.com>
2023-11-10 01:08:05 +0300

    Başlangıç

---
+++ OgrenciKursApp/Data/OGrenci.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OgrenciKursApp.Data
+{
+    public class OGrenci
+    {
+        [Key]
+        public int OgrenciId { get; set; }
+        public string? OgnreciAd { get; set; }
+        public string? OgnreciSoyad { get; set; }
+        public string? Eposta { get; set; }
+        public string? Telefon { get; set; }
+       
+    }
+}
