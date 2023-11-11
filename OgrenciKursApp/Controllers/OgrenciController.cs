4fbeb6f147cbea75c8868eb40bdcbcacaf56d159
Zahid Buğra Sarıcan <zbsarican@gmail.com>
2023-11-12 00:06:34 +0300

    Edit/Kayıt Güncelleme İşlemi yaptım.

--- OgrenciKursApp/Controllers/OgrenciController.cs
+++ OgrenciKursApp/Controllers/OgrenciController.cs
@@ -28,6 +28,58 @@ namespace OgrenciKursApp.Controllers
             return View();
         }
 
+        //Edit metotu. ID'ye erişemezse, geçiyor varsa giriyor ve bilgilerini de atıyor. FindAsync veya FirstOrDefaultAsync ile kayıtları check'liyorum.
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id )
+        {
+            if (id==null)
+            {
+                return NotFound();
+            }
+           
+            var ogrenci= await _context.Ogrenciler.FindAsync(id);
+           //var ogrenci= await _context.Ogrenciler.FirstOrDefaultAsync(o=> o.OgrenciId== id);
+           
+            if (ogrenci==null)
+            {
+                return NotFound();
+            }
+            return View(ogrenci);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Ogrenci model)
+        {
+            if (id != model.OgrenciId)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(model);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+
+                    if (!_context.Ogrenciler.Any(o=>o.OgrenciId==model.OgrenciId))    //öğrenci bulanamadı ise
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");   
+            }
+            return View(model);
+
+
+        }
 
         //Gelen verileri db'ye gönderdim.
         [HttpPost]
