7aac06d4af6fa37a35677a762f354ffb15a80499
Zahid Buğra Sarıcan <zbsarican@gmail.com>
2023-11-13 00:09:03 +0300

    KursKayit Controller'ı açtım. Kurs için create metodu yazdım.

---
+++ OgrenciKursApp/Controllers/KursKayitController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OgrenciKursApp.Data;
+
+namespace OgrenciKursApp.Controllers
+{
+    public class KursKayitController : Controller
+    {
+        private readonly DataContext _context;
+        public KursKayitController(DataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Ogrenciler= new SelectList(await _context.Ogrenciler.ToListAsync(), "OgrenciId", "OgrenciAd");
+            ViewBag.Kurslar= new SelectList(await _context.Kurslar.ToListAsync(), "KursId", "Baslik");
+            
+            return View();
+        }
+
+        
+    }
+}
